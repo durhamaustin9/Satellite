9af889d771d28c1091103b044584544fd97d136d
Austin Durham <austindurham@Austins-MBP.lan>
2022-04-16 17:16:52 -0700

    Added search by name capability

--- routes/satellite.js
+++ routes/satellite.js
@@ -2,43 +2,51 @@ const path = require('path')
 const router = require('express').Router()
 const controllers = require(path.join(process.cwd(), '/controllers'))
 
-router.get('/tle', async (request, response) => {
-  controllers.satellitePosition.doGetTwoLineElement(request.body.id).then(data => {
+router.get('/search', async (request, response) => {
+  controllers.satellitePosition.doSearch(request.body.name).then(data => {
     response.status(200).json(data)
   }).catch(_ => {
-    response.status(201).json(new Error())
+    response.status(500).json()
   })
 })
 
-router.get('/position', async (request, response) => {
-  controllers.satellitePosition.doGetSatellitePosition(request.body.id, request.body.latitude, request.body.longitude, request.body.elevation, request.body.result).then(data => {
+router.get('/tle/:input', async (request, response) => {
+  controllers.satellitePosition.doGetTwoLineElement(request.params.input).then(data => {
     response.status(200).json(data)
   }).catch(_ => {
-    response.status(201).json(new Error())
+    response.status(500).json()
   })
 })
 
-router.get('/visualPasses', async (request, response) => {
-  controllers.satellitePosition.doGetVisualPasses(request.body.id, request.body.latitude, request.body.longitude, request.body.elevation, request.body.days, request.body.visibleLength).then(data => {
+router.get('/position/:input', async (request, response) => {
+  controllers.satellitePosition.doGetSatellitePosition(request.params.input, request.query.latitude, request.query.longitude, request.query.elevation, request.query.resultLength).then(data => {
     response.status(200).json(data)
   }).catch(_ => {
-    response.status(201).json(new Error())
+    response.status(500).json()
   })
 })
 
-router.get('/radioPasses', async (request, response) => {
-  controllers.satellitePosition.doRadioPasses(request.body.id, request.body.latitude, request.body.longitude, request.body.elevation, request.body.days, request.body.minimumElevation).then(data => {
+router.get('/visualPasses/:input', async (request, response) => {
+  controllers.satellitePosition.doGetVisualPasses(request.params.input, request.query.latitude, request.query.longitude, request.query.elevation, request.query.days, request.query.visibleLength).then(data => {
     response.status(200).json(data)
   }).catch(_ => {
-    response.status(201).json(new Error())
+    response.status(500).json()
+  })
+})
+
+router.get('/radioPasses/:input', async (request, response) => {
+  controllers.satellitePosition.doRadioPasses(request.params.input, request.query.latitude, request.query.longitude, request.query.elevation, request.query.days, request.query.minimumElevation).then(data => {
+    response.status(200).json(data)
+  }).catch(_ => {
+    response.status(500).json()
   })
 })
 
 router.get('/radialPasses', async (request, response) => {
-  controllers.satellitePosition.doRadialPasses(request.body.latitude, request.body.longitude, request.body.elevation, request.body.radius, request.body.category).then(data => {
+  controllers.satellitePosition.doRadialPasses(request.query.latitude, request.query.longitude, request.query.elevation, request.query.radius, request.query.category).then(data => {
     response.status(200).json(data)
   }).catch(_ => {
-    response.status(201).json(new Error())
+    response.status(500).json()
   })
 })
 
